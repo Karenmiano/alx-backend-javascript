83ed97d3be39f008becef40fa87689dcdf14aeb5
Karenmiano <mianokaren1798@gmail.com>
2024-04-09 06:21:15 +0300

    for...of

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    let arr = [];
+    for (let value of array) {
+        arr.push(appendString + value);
+    }
+  
+    return arr;
+}
