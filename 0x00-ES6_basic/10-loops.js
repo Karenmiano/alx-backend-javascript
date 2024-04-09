8b5ed051d1c397209c3ddf9da563217dc49d6086
Karenmiano <mianokaren1798@gmail.com>
2024-04-09 13:18:13 +0300

    modified loop to use for...of instead of for...in for array

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,8 +1,8 @@
 export default function appendToEachArrayValue(array, appendString) {
-    let arr = [];
-    for (let value of array) {
-        arr.push(appendString + value);
-    }
-  
-    return arr;
+  const arr = [];
+  for (const value of array) {
+    arr.push(appendString + value);
+  }
+
+  return arr;
 }
