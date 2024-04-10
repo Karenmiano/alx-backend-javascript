579b4f5f2cae7dff59101163c1c3f4680720bf20
Karenmiano <mianokaren1798@gmail.com>
2024-04-10 07:21:01 +0300

    create a readable of Employees from object iterator

--- 0x00-ES6_basic/trial.js
+++ 0x00-ES6_basic/trial.js
@@ -1,25 +1,27 @@
 export default function createIteratorObject(report) {
-  report[Symbol.iterator] = function iter() {
-    let n = 0;
-    const employees = [];
-    for (const key in this.allEmployees) {
-      employees.push(...this.allEmployees[key]);
-    }
+  const newObj = { 
+    ...report,
+    [Symbol.iterator]:  function iter() {
+      let n = 0;
+      const employees = [];
+      for (const key in this.allEmployees) {
+        employees.push(...this.allEmployees[key]);
+      }
 
-    return {  
-      next() {
-        if (n < employees.length) {
+      return {
+        next() {
+          if (n < employees.length) {
+            const temp = { value: employees[n], done: false };
+            n += 1;
+            return temp;
+          }
           return {
-            value: employees[n++],
-            done: false,
+            value: undefined,
+            done: true,
           };
-        }
-        return {
-          value: undefined,
-          done: true,
-        };
-      },
-    };
+        },
+      };
+    },
   };
-  return report;
+  return newObj;
 }
