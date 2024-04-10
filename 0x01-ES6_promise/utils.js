edacc3ac0e214469e2a8ffa511014406ef2c2d90
Karenmiano <mianokaren1798@gmail.com>
2024-04-10 19:41:05 +0300

    use promise.all() to handle multiple promises

---
+++ 0x01-ES6_promise/utils.js
@@ -0,0 +1,14 @@
+export function uploadPhoto() {
+  return Promise.reject({
+    status: 200,
+    body: 'photo-profile-1',
+  });
+}
+
+
+export function createUser() {
+  return Promise.resolve({
+    firstName: 'Guillaume',
+    lastName: 'Salva',
+  });
+}
