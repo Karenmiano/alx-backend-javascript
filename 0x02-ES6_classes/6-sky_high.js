653b1d2e76ecd86dce1101ef2c0e1536742cb9f7
Karenmiano <mianokaren1798@gmail.com>
2024-04-12 01:04:57 +0300

    created class SkyHighBuilding that extends Building

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,23 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(floors) {
+    if (typeof floors !== 'number') {
+      throw new Error('Floors must be a number');
+    }
+    this._floors = floors;
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly ${this.floors} floors.`;
+  }
+}
