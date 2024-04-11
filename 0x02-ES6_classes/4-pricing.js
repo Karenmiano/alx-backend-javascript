46bb71f9b58b8d3827a0830a310543f4fbf160cd
Karenmiano <mianokaren1798@gmail.com>
2024-04-11 23:01:00 +0300

    created Pricing class that implements convertPrice static method

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,41 @@
+import Currency from './3-currency';
+
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  //Getters and setters for amount
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    if (typeof amount !== 'number') {
+      throw new TypeError('Amount must be a number');
+    }
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    if (!currency instanceof Currency) {
+      throw new TypeError('currency must be an instance of Currency');
+    }
+    this._currency = currency;
+  }
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (typeof amount !== 'number' || typeof conversionRate !== 'number') {
+      throw new Error('Amount and conversionRate must be numbers');
+    }
+    return amount * conversionRate;
+  }
+}
