1afc430df9137fb18e15a930f04cc663f10be353
Karenmiano <mianokaren1798@gmail.com>
2024-04-11 03:03:39 +0300

    using Promise.allSettled to return a resolved promise no matter the state of promises passed

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -5,5 +5,9 @@ export default function handleProfileSignUp(firstName, lastName, fileName) {
   const promise1 = signUpUser(firstName, lastName);
   const promise2 = uploadPhoto(fileName);
 
-  return Promise.allSettled([promise1, promise2]);
+  return Promise.allSettled([promise1, promise2])
+    .then((result) => result.map(({ status, value, reason }) => ({
+      status,
+      value: status === 'fulfilled' ? value : reason.message,
+    })));
 }
