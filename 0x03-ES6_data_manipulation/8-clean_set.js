6fc516a297d37d97d7c5a9ec382cc74619b9ce0a
Karenmiano <mianokaren1798@gmail.com>
2024-04-20 00:56:53 +0300

    clean set

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,5 +1,8 @@
 export default function cleanSet(set, startString) {
-  return startString.length === 0 ? '' : Array.from(set)
+  if (typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+  return Array.from(set)
     .filter((item) => item.startsWith(startString))
     .map((item) => item.slice(startString.length))
     .join('-');
